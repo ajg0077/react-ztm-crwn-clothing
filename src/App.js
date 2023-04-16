cf4ec87207fac835d3b47cc1155b407ffb3e2a1c
Antony George <antonyjoy.george@gmail.com>
2023-04-16 13:15:20 +0530

    Closes #10; Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navigation Bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+      </Route>
     </Routes>
   );
 };
