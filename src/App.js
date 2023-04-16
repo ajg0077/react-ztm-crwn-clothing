42dd1b1130a8cd42dc609c0ef16d28335525fecb
Antony George <antonyjoy.george@gmail.com>
2023-04-16 13:40:37 +0530

    Closes #11; Adding navigation Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from "./components/routes/navigation/navigation.component";
 
 const App = () => {
   return (
