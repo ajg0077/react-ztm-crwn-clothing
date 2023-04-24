7bc7581c256eb466c0777139274bf59e4182170a
Antony George <antonyjoy.george@gmail.com>
2023-04-24 17:03:56 +0530

    remove Redirect

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
 import {
-  auth,
   signInWithGooglePopup,
   signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
-import { getRedirectResult } from "firebase/auth";
 const SignIn = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    };
-    fetchData();
-  }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -24,9 +12,6 @@ const SignIn = () => {
     <div>
       <h1>SignIn</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
