5b4d3b0c7399669435484f2afa7a33a911438005
Antony George <antonyjoy.george@gmail.com>
2023-04-26 18:30:45 +0530

    Sign In with Email and Password Closes #22;

---
+++ src/components/routes/authentication/authentication.component.jsx
@@ -0,0 +1,13 @@
+import SignInForm from "../../sign-in-form/sign-in-form.component";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
+
+const Authentication = () => {
+  return (
+    <div>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
